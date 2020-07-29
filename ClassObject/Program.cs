29864fc09c5f314e4d1c10fa34e3d56829dbfd91
rafifauzan09 <rafif.almahdy@students.amikom.ac.id>
2020-07-30 03:39:22 +0700

    fix

---
+++ ClassObject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Penerapan Class dan Object\n");
+
+            bangunan rumah = new bangunan();
+            bangunan kos = new bangunan();
+
+            rumah.pemilik = "Ari Wicaksono";
+            rumah.jenisb = "Rumah";
+            rumah.alamat = "Cendrawasih 71";
+            rumah.luas = 96;
+
+            rumah.identitas();
+            rumah.proses();
+
+            kos.pemilik = "Rangga Tasyim";
+            kos.jenisb = "Kos";
+            kos.alamat = "Cendrawasih 112";
+            kos.luas = 144;
+
+            kos.identitas();
+            kos.selesai();
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ ClassObject/bangunan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassObject
+{
+    public class bangunan
+    {
+        //properties
+        public string pemilik { get; set; }
+        public int luas { get; set; }
+        public string alamat { get; set; }
+        public string jenisb { get; set; }
+
+        //method
+        public void identitas()
+        {
+            Console.WriteLine("Nama Pemilik : {0}", pemilik);
+            Console.WriteLine("Luas Bangunan : {0}m2", luas);
+            Console.WriteLine("Alamat : {0}", alamat);
+        }
+
+        public void proses()
+        {
+            Console.WriteLine("\n{0} {1}, sedang dalam pembangunan\n\n",jenisb, pemilik);
+        }
+        public void selesai()
+        {
+            Console.WriteLine("\n{0} {1},selesai dibangun\n\n",jenisb, pemilik);
+        }
+    }
+}
